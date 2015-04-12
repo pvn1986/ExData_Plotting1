81c47cf03ac18bef7ff490e0cbef574c88f33027
pvn1986 <pvn1986@gmail.com>
2015-04-11 17:59:15 -0700

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,33 @@
+# Exploratory Data Analysis
+# Course Project 1
+# Base plot
+
+#Read Data
+
+library(data.table)
+
+# Read all data
+all_data <- fread("household_power_consumption.txt",header=TRUE)
+
+# Filter required date range
+data <- all_data[as.Date(all_data$Date,'%d/%m/%Y')==as.Date("2007-02-01") | as.Date(all_data$Date,'%d/%m/%Y') == as.Date("2007-02-02"),]
+
+# Convert to Date Time
+data[,DT:=as.POSIXct(strptime(paste(data$Date,data$Time),format='%d/%m/%Y %H:%M:%S'))]
+
+# convert Not Null Values of global active power
+active_power<-as.numeric(data$Global_active_power[data$Global_active_power!='?'])
+
+# Histogram
+hist(active_power,
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)" ,
+     col="red")
+
+
+#Copy to File
+dev.copy(png, file="plot1.png", width=480, height=480)
+
+dev.off()
+rm(all_data)
+rm(data)
